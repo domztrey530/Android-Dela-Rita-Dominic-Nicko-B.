35ba51d10de5b2e82b50419d7f56ce31198a0572
Dominic Nicko Dela Rita <nick3delta@gmail.com>
2023-02-11 13:03:37 +0800

    Initial Main Activity

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mainactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is a verbose log.");
+        Log.d(TAG, "This is a debug log.");
+        Log.i(TAG, "This is an info log.");
+        Log.w(TAG, "This is a warm log.");
+        Log.e(TAG, "This is an error log.");
     }
 }
