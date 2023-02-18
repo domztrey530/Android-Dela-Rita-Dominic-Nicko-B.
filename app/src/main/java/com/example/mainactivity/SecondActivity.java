513bbd3714889793bb75fa82944d890cb256ad0a
Dominic Nicko Dela Rita <nick3delta@gmail.com>
2023-02-18 12:40:17 +0800

    February 18, 2023, Third Activity

--- app/src/main/java/com/example/mainactivity/SecondActivity.java
+++ app/src/main/java/com/example/mainactivity/SecondActivity.java
@@ -2,13 +2,30 @@ package com.example.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button secondactbtn = (Button) findViewById(R.id.secondactbtn);
+        secondactbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/mainactivity/ThirdActivity.java
@@ -0,0 +1,26 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        Button sendbutton = (Button) findViewById(R.id.sendbutton);
+        sendbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ThirdActivity.this, FourthActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
