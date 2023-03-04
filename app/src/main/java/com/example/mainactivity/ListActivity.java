48a0574e7a88cfa286bce7cb578586cbf24defbc
Dominic Nicko Dela Rita <nick3delta@gmail.com>
2023-03-04 14:24:57 +0800

    LIST VIEW

---
+++ app/src/main/java/com/example/mainactivity/ListActivity.java
@@ -0,0 +1,33 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterViewAnimator;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListActivity extends AppCompatActivity {
+    ListView listView;
+    TextView textView;
+
+    String[] listItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        listView = (ListView) findViewById(R.id.listview);
+        listItem = getResources().getStringArray(R.array.array_technology);
+//        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.myList, R.id.textView, listItem);
+//        listView.setAdapter(adapter);
+
+
+    }
+}
+
