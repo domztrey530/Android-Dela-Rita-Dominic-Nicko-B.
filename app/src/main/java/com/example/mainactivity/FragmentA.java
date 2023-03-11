314ab509053c2f432d61ce8619cfee0a765caa8f
Dominic Nicko Dela Rita <nick3delta@gmail.com>
2023-03-11 12:40:37 +0800

    FRAG ACTIVITY REVISE2

--- app/src/main/java/com/example/mainactivity/FragmentA.java
+++ app/src/main/java/com/example/mainactivity/FragmentA.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.app.Fragment;
+import androidx.fragment.app.Fragment;
 
 
 /**
--- app/src/main/java/com/example/mainactivity/MainFrag.java
+++ app/src/main/java/com/example/mainactivity/MainFrag.java
@@ -2,9 +2,10 @@ package com.example.mainactivity;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import android.app.Fragment;
+
 
 //import android.app.FragmentManager;
 
