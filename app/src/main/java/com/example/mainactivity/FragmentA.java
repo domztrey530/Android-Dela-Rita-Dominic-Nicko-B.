c5be049726b18657ab93ed30b4800f4dde226e4d
Dominic Nicko Dela Rita <nick3delta@gmail.com>
2023-03-11 12:23:05 +0800

    FRAG ACTIVITY REVISE

--- app/src/main/java/com/example/mainactivity/FragmentA.java
+++ app/src/main/java/com/example/mainactivity/FragmentA.java
@@ -2,11 +2,13 @@ package com.example.mainactivity;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.app.Fragment;
+
 
 /**
  * A simple {@link Fragment} subclass.
--- app/src/main/java/com/example/mainactivity/MainFrag.java
+++ app/src/main/java/com/example/mainactivity/MainFrag.java
@@ -1,14 +1,14 @@
 package com.example.mainactivity;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
 import android.app.Fragment;
-//import android.app.FragmentManager;
-//import android.app.FragmentTransaction;
 
+//import android.app.FragmentManager;
+
+//import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,8 +32,25 @@ public class MainFrag extends AppCompatActivity
             {
             @Override
             public void onClick(View v) {
+                loadFragment(new FragmentA());
+            }
+        });
 
-            });
-        }
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentB());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment)
+    {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fm.beginTransaction();
+
+        FragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
+
     }
 }
