a7bfedd4e49f7fd3db6160a8f4e095af5696fd83
Dominic Nicko Dela Rita <nick3delta@gmail.com>
2023-04-29 13:27:51 +0800

    FINAL Database and Shared Preferences Activity

--- database/src/main/java/com/example/database/MainActivity.java
+++ database/src/main/java/com/example/database/MainActivity.java
@@ -33,6 +33,10 @@ public class MainActivity extends AppCompatActivity
         //Get data
         getData();
 
+        //Update Data
+        dbHelper.updateData(2, "Sikoa", 22);
+        getData();
+
         //Delete Data
         dbHelper.deleteData(2);
 
